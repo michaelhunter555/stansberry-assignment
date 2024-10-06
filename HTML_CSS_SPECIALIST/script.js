189dbc7331cd716bf4c0f1f7b1be158a694d08e6
michaelhunter555 <dumbfreshdope@gmail.com>
2024-10-05 20:17:11 -0400

    initialize repository for stansberry assignment

---
+++ HTML_CSS_SPECIALIST/script.js
@@ -0,0 +1,21 @@
+//e-mail validation check
+const validateEmail = (email) => {
+  return /^\S+@\S+\.\S+$/.test(email);
+};
+//form submission
+const handleFormSubmit = (event) => {
+  event.preventDefault();
+  //retrieve e-mail input
+  let value = event.target.querySelector('input[type="email"]').value;
+  //validate e-mail
+  const emailIsValid = validateEmail(value);
+  //redirect to thank you page if email is valid
+  if (emailIsValid) {
+    window.location.href = "/HTML_CSS_SPECIALIST/thankyou.html";
+    //clear form
+    event.target.reset();
+  } else {
+    //alert user their e-mail is invalid
+    alert("Your email is not valid. Please enter a valid email format.");
+  }
+};
